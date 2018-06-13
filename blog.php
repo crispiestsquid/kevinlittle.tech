8b908e985400cc3f6956d7ba3c5e752f8a7b6804
Kevin Little <me@kevinlittle.tech>
2018-06-13 11:06:11 -0400

    minor changes and updates

--- blog.php
+++ blog.php
@@ -37,7 +37,7 @@ require( 'wp_blog/wp-blog-header.php' );
 					</li>
 					<li><a class="current" href="blog.php">Blog</a>
 					</li>
-					<li><a href="about.html">About</a>
+					<li><a href="projects.html">Projects</a>
 					</li>
 					<li><a href="contact.html">Contact</a>
 					</li>
@@ -63,7 +63,7 @@ require( 'wp_blog/wp-blog-header.php' );
 					</li>
 					<li><a class="current" href="blog.php">Blog</a>
 					</li>
-					<li><a href="about.html">About</a>
+					<li><a href="projects.html">Projects</a>
 					</li>
 					<li><a href="contact.html">Contact</a>
 					</li>
